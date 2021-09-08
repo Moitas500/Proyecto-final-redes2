c977c6b7282a69f9a7e9c18ac8c151d2cfd67ae4
Moitas500 <64492570+Moitas500@users.noreply.github.com>
2021-09-08 13:58:31 -0500

    Algoritmo Dijkstra implementado

---
+++ Graph.py
@@ -0,0 +1,55 @@
+from queue import PriorityQueue
+
+class Graph:
+
+    def add_edge(self, u, v, weight):
+         self.edges[u][v] = weight
+         self.edges[v][u] = weight
+
+    def __init__(self, num_of_vertices):
+        self.v = num_of_vertices
+        self.edges = [[-1 for i in range(num_of_vertices)] for j in range(num_of_vertices)]
+        self.visited = []
+
+    def dijkstra(self, start_vertex):
+        D = {v:float('inf') for v in range(self.v)}
+        D[start_vertex] = 0
+
+        pq = PriorityQueue()
+        pq.put((0, start_vertex))
+
+        while not pq.empty():
+            (dist, current_vertex) = pq.get()
+            self.visited.append(current_vertex)
+
+            for neighbor in range(self.v):
+                if self.edges[current_vertex][neighbor] != -1:
+                    distance = self.edges[current_vertex][neighbor]
+                    if neighbor not in self.visited:
+                        old_cost = D[neighbor]
+                        new_cost = D[current_vertex] + distance
+                        if new_cost < old_cost:
+                            pq.put((new_cost, neighbor))
+                            D[neighbor] = new_cost
+        return D
+
+    def print_bellman(self, dist):
+        print("Vertex Distance from Source")
+        for i in range(self.v):
+            print("{0}\t\t{1}".format(i, dist[i]))
+
+    def bellman_ford(self, src):
+        dist = [float("Inf")] * self.v
+        dist[src] = 0
+
+        for _ in range(self.v - 1):
+            for s, d, w in self.graph:
+                if dist[s] != float("Inf") and dist[s] + w < dist[d]:
+                    dist[d] = dist[s] + w
+
+        for s, d, w in self.edges:
+            if dist[s] != float("Inf") and dist[s] + w < dist[d]:
+                print("Graph contains negative weight cycle")
+                return
+
+        self.print_solution(dist)
---
+++ main.py
@@ -0,0 +1,29 @@
+from Graph import Graph
+
+def main():
+    g = Graph(9)
+    g.add_edge(0, 1, 4)
+    g.add_edge(0, 6, 7)
+    g.add_edge(1, 6, 11)
+    g.add_edge(1, 7, 20)
+    g.add_edge(1, 2, 9)
+    g.add_edge(2, 3, 6)
+    g.add_edge(2, 4, 2)
+    g.add_edge(3, 4, 10)
+    g.add_edge(3, 5, 5)
+    g.add_edge(4, 5, 15)
+    g.add_edge(4, 7, 1)
+    g.add_edge(4, 8, 5)
+    g.add_edge(5, 8, 12)
+    g.add_edge(6, 7, 1)
+    g.add_edge(7, 8, 3)
+
+    D = g.dijkstra(0)
+
+    for vertex in range(len(D)):
+        print("Distance from vertex 0 to vertex", vertex, "is", D[vertex])
+
+    g.bellman_ford(0)
+
+if __name__ == "__main__":
+    main()
